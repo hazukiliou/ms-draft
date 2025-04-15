723554b4e445ff9b5887306170f8efe71ec77029
hazuki <hazuki.liou@gmail.com>
2025-04-15 21:30:04 +0800

    chore(config): setup eslint, prettier and pre-commit hooks
    - 整合 Prettier
    - 增加 pre-commit hook

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  assetPrefix: isProd ? '/ms-draft/' : '',
+  basePath: isProd ? '/ms-draft' : '',
+  trailingSlash: true,
+}
 
-export default nextConfig;
+export default nextConfig
